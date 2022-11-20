72e27db248cf103f8dbd469ad8223c8e39c18035
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 16:13:58 -0500

    End of lesson 3

---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Products</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, autem cupiditate ea excepturi itaque iusto
+    natus non optio porro quae quidem similique ut voluptatem. Asperiores labore nesciunt rerum saepe voluptas?</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut cum cumque natus nisi odio quis quod tempore,
+    voluptas voluptatum?</p>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
