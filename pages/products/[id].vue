9fbb9ed5215befb3b4c6744099c96c537ae70367
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 16:22:42 -0500

    End of lesson 4

---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Products details for {{ id }}</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam cum dolorum, excepturi fuga hic illum impedit
+    in iusto magni, modi molestiae nisi, praesentium quam quisquam quod sint tenetur vitae!</p>
+</template>
+
+<script setup>
+const {id} = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
