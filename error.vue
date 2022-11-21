2f569d3cdb10b661b2bf66e302788a40905256ce
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 20:17:02 -0500

    Create custom error page

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <h1 class="mt-7 text-7x1 font-bold">
+      {{ error.statusCode }}
+    </h1>
+    <p class="mt-7 text-6x1">
+      Ooops.
+    </p>
+    <p class="mt-7">
+      {{ error.message }}
+    </p>
+    <button class="btn my-7" @click="handleClearError">Go Home</button>
+  </div>
+</template>
+
+<script setup>
+defineProps(['error'])
+
+const handleClearError = () => clearError({
+  redirect: '/'
+})
+</script>
+
+<style scoped>
+
+</style>
