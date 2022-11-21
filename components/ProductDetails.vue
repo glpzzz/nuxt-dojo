9c5741527182ef2a819569e9207d5a231c15f45b
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 20:30:27 -0500

    Add global external stylesheet for Material Icons and "Add to Cart" button on `ProductDetails`

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -13,6 +13,10 @@
           {{ product.category }}</p>
         <h2 class="font-bold border-b-2 mb-4 pb-2">Description</h2>
         <p>{{ product.description }}</p>
+        <button class="btn flex">
+          <i class="material-icons mr-2">add_shopping_cart</i>
+          <span>Add to cart</span>
+        </button>
       </div>
     </div>
   </article>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
             meta: [
                 {name: 'description', content: 'Everything about Nuxt 3'},
             ],
+            link: [
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
+            ]
         }
     }
 })
