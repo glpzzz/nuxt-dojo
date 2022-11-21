0e33b76873b52e7eee1d348b3dd85f278e6fc43c
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 20:17:31 -0500

    Throw error when no product fetch from API

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,5 @@
 <template>
-  <ProductDetails v-if="product" :product="product" />
+  <ProductDetails v-if="product" :product="product"/>
 </template>
 
 <script setup>
@@ -9,6 +9,12 @@ definePageMeta({
 const {id} = useRoute().params
 const uri = `https://fakestoreapi.com/products/${id}`
 const {data: product} = await useFetch(uri, {key: id});
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Product not found',
+  })
+}
 </script>
 
 <style scoped>
