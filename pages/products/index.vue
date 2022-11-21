b194996e202edd1ae1b1ea48432ee07c6452928d
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 20:32:49 -0500

    Override page metatags with `useHead()`

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Products</h1>
   <ul class="grid grid-cols-4 gap-5">
-    <ProductCard v-for="p in products" :product="p" />
+    <ProductCard v-for="p in products" :product="p"/>
   </ul>
 </template>
 
@@ -12,8 +12,12 @@ definePageMeta({
 
 // fetch products
 const {data: products} = await useFetch('https://fakestoreapi.com/products')
-
-
+useHead({
+  title: 'Our products',
+  meta: [
+    {name: 'description', content: 'Nuxt 3 Merch'},
+  ]
+})
 </script>
 
 <style scoped>
